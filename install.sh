2d130efe4105603429d441f202f5d769548300d0
Parker Smith <parker@parker.ws>
2019-12-13 15:08:27 -0600

    install.sh
    Run this script ./install.sh after git cloning the repository to pull and install Polyglot-v2 and Mongo

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-mkdir -p ./mongodb/
 
+echo "Installing polyglot-v2 and mongo..." &
+echo "Running docker-compose up..." &
+process_id=$!
 sudo docker-compose up -d
+wait $process_id
+echo "Creating SSL certs & stopping containers..." &
 sudo docker-compose stop -d
+wait $process_id
+echo "Starting containers..." &
+wait $process_id
 sudo docker-compose up -d
+wait $process_id
+echo "Polyglot v2 should now be running in Docker! Access via webbrowser at https://{docker ip address}:3000"
